6ea5f9f1bf94b142a44609eedc5fddce31f7cf6c
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-01-18 21:40:03 -0500

    Wrote initial grammar

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+#[derive(Parser)]
+#[grammar="grammar488.pest"]
+pub struct CSC488Parser;
+
 fn main() {
     println!("Hello world!");
 }
