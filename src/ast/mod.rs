748bbbc657e72f405017cd2af4c91046418d3054
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-01-19 00:52:01 -0500

    Added variable types

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -9,12 +9,28 @@ trait Callable {
 }
 
 pub struct Variable {
+    var_type : Type
+}
 
+impl Variable {
+    pub fn new(var_type : Type) -> Variable {
+        Variable{var_type : var_type}
+    }
+}
+
+impl Typed for Variable {
+    fn get_type(&self) -> Type {return self.var_type.clone()}
 }
 
 pub struct Function {
     arity : u32,
     ret_type : Type
+}
+
+impl Function {
+    pub fn new(arity : u32, ret_type : Type) -> Function {
+        Function{arity : arity, ret_type : ret_type}
+    }
 }
 
 impl Callable for Function {
