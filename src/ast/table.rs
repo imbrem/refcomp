34f8605d1190864301289a7864126bb8d2db91fc
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-01-19 02:51:45 -0500

    Further work on symbol table implementation

--- src/ast/table.rs
+++ src/ast/table.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use super::types::{Type, Typed};
 use super::statement::Scope;
 use std::collections::BTreeMap as SymbolMap;
@@ -16,6 +17,7 @@ impl Variable {
     pub fn new(name : String, var_type : Type) -> Variable {
         Variable{name : name, var_type : var_type}
     }
+    pub fn get_name(&self) -> &str {&self.name}
 }
 
 impl Typed for Variable {
@@ -36,6 +38,7 @@ impl Function {
         name : name, arity : arity, ret_type : ret_type,
         fn_impl : Some(fn_impl)
     }}
+    pub fn get_name(&self) -> &str {&self.name}
 }
 
 impl Callable for Function {
@@ -57,20 +60,31 @@ impl Procedure {
     pub fn new(name : String, arity : u32, pr_impl : Scope) -> Procedure {
         Procedure{name : name, arity : arity, pr_impl : Some(pr_impl)}
     }
+    pub fn get_name(&self) -> &str {&self.name}
 }
 
 impl Callable for Procedure {
     fn get_arity(&self) -> usize {self.arity as usize}
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum Symbol {
-    Variable(Variable),
-    Function(Function),
-    Procedure(Procedure)
+    Variable(Rc<Variable>),
+    Function(Rc<Function>),
+    Procedure(Rc<Procedure>)
 }
 
-#[derive(PartialEq, Debug)]
+impl Symbol {
+    pub fn get_name(&self) -> &str {
+        match self {
+            Symbol::Variable(v) => v.get_name(),
+            Symbol::Function(f) => f.get_name(),
+            Symbol::Procedure(p) => p.get_name()
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
 pub struct SymbolTable {
     symbols : SymbolMap<String, Symbol>
 }
@@ -79,5 +93,17 @@ impl SymbolTable {
     pub fn new() -> SymbolTable {SymbolTable{symbols : SymbolMap::new()}}
     pub fn with_init(symbols : SymbolMap<String, Symbol>) -> SymbolTable {
         SymbolTable{symbols : symbols}
+    }
+    pub fn dereference(&self, symbol : &str) -> Option<Symbol> {
+        match self.symbols.get(symbol) {
+            Some(r) => Some(r.clone()),
+            None => None
+        }
+    }
+    pub fn define(&mut self, symbol : Symbol) -> Option<Symbol> {
+        self.reference(symbol.get_name().to_string(), symbol)
+    }
+    pub fn reference(&mut self, name : String, symbol : Symbol) -> Option<Symbol> {
+        self.symbols.insert(name, symbol)
     }
 }
